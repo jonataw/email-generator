49bd133fb68974f0b121b1ee4c5388ff8bcceb6f
Jonatan Wackström <29772763+Asgarv@users.noreply.github.com>
2020-07-19 01:45:16 +0300

    Add .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+module.exports = {
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always'
+};
