c38dcfc232560df1535860ad1d01db052ac9b058
Jonatan Wackström <29772763+Asgarv@users.noreply.github.com>
2020-07-19 21:47:19 +0300

    Add minify option

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import InlineCss from 'inline-css';
 import { SassCompiler } from './sass';
+import { minify } from 'html-minifier';
+import config from './config';
 
 class EmailGenerator {
   private readonly SOURCE_DIR = './src/templates';
@@ -19,13 +21,19 @@ class EmailGenerator {
     files.forEach(async (file) => {
       try {
         const html = fs.readFileSync(this.SOURCE_DIR + '/' + file, 'utf8');
-        const inlined = await InlineCss(html, {
+        let result = await InlineCss(html, {
           url: 'file://' + __dirname + '/' + path.dirname(file) + '/styles/'
         });
 
+        if (config.minify) {
+          result = minify(result, { collapseWhitespace: true });
+        }
+
         fs.writeFile(
-          './dist/' + file.substring(0, file.lastIndexOf('.')) + '.html',
-          inlined,
+          './dist/' +
+            file.substring(0, file.lastIndexOf('.')) +
+            file.substring(file.lastIndexOf('.'), file.length),
+          result,
           function (err) {
             if (err) {
               return console.error(err);
